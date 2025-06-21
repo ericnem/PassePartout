60e3d8072143283846f8a9e1f3b1dbb12685d57b
ericnem <ericnem@outlook.com>
2025-06-21 16:17:08 -0400

    Change name

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import Component from "./Main"; // adjust the path if needed
+import MainPage from "./Main"; // adjust the path if needed
 
 function App() {
   return ( 
     <div>
-      <Component />
-      <Component />
+      <MainPage />
     </div>
 
   );
