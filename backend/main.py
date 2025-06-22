4caccc7269114f28c185d2e306f6dce549030a49
antonnemch <104526851+antonnemch@users.noreply.github.com>
2025-06-22 17:20:51 -0400

    final final final final final final final final commit

--- backend/main.py
+++ backend/main.py
@@ -35,7 +35,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:5173",
-        "https://passepartout-live.onrender.com"
+        "https://passepartout-live.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
